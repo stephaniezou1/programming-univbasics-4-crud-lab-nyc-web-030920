c21a0b731dc86a43a2b48c14c050315c2d4d06b1
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-13 18:27:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  my_array = Array.new
 end
 
 def create_an_array
-  
+  my_array = ["a", 1,2,3]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
